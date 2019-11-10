6cb9ee10ad0200c99faf5c9cb48f59444f19ecd3
Ella Abraham <48738996+EllaAb@users.noreply.github.com>
2019-11-09 23:36:47 -0600

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import Background from "../Img/bgella.jpg" ;
 import '../styles/MainPage.css';
-//import {Icon } from "semantic-ui-react";
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedinIn, faInstagram, faGithub, faTwitter, faFacebook,  } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn, faInstagram, faGithub, faTwitter, faFacebook  } from '@fortawesome/free-brands-svg-icons';
 
 
 const myStyles = {
     backgroundImage: `url( ${Background} )`,
     height: '80vh',
-  
     backgroundSize: 'cover'
-
 } 
 
 const iconStyle = {marginLeft: 15, marginRight: 15, color: "#f37335"};
@@ -30,42 +26,18 @@ class AboutMe extends React.Component {
                 Passionate about coding unique websites,<br /> that clearly communicate with visitors.   </h4>
 
 
-              
-               
-
               <div className='text-center'>
 
-               <a target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/ella-abraham-3b5274169/" style={iconStyle}><FontAwesomeIcon icon={faLinkedinIn} size='lg'/></a>
+          <a target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/ella-abraham-3b5274169/" style={iconStyle}><FontAwesomeIcon icon={faLinkedinIn} size='lg'/></a>
           <a target='_blank' rel='noopener noreferrer' href='https://twitter.com' style={iconStyle}><FontAwesomeIcon icon={faTwitter} size='lg'/></a>
           <a target='_blank' rel='noopener noreferrer' href='https://github.com/EllaAb' style={iconStyle}><FontAwesomeIcon icon={faGithub} size='lg'/></a>
           <a target='_blank' rel='noopener noreferrer' href='https://instagram.com' style={iconStyle}><FontAwesomeIcon icon={faInstagram} size='lg'/></a>
           <a target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/ula.qasim' style={iconStyle}><FontAwesomeIcon icon={faFacebook} size='lg'/></a>
          
-        </div>      
-
-                
-               
-
-               
-               
-                    
-
+        </div>        
             </header>    
         );
     }
 }
 
 export default AboutMe;
-
-/* <a href="youtube.com"  >
-                    <Icon className="huge git square" />
-                    </a>
-
-                    <a href="youtube.com"  >
-                    <Icon className="huge id card" />
-                    </a> 
-                    <div className="social-links">
-                    <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                <i className="fa fa-linkedin-square" aria-hidden="true" />
-              </a>
-              </div>*/
