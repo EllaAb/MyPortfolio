47dfa2dfe4ea581f477d7d2a7d050d8097dd6fcf
Ella Abraham <48738996+EllaAb@users.noreply.github.com>
2019-11-09 23:22:00 -0600

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,41 +1,69 @@
 import React from 'react';
-import {Tabs,Tab} from 'react-mdl';
+//import {Tabs,Tab} from 'react-mdl';
+import projects from  "../Img/code3.png";
+import "../styles/Projects.css";
+
 
 class Projects extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { activeTab: 0 };
-      }
-
-      toggleCategories() {
-
-          if(this.state.activeTab === 0) {
-              return (
-           <div>  <h1>This Is html</h1></div> 
-              )
-          } else if(this.state.activeTab ===1) {
-              return (
-                  <div><h1>This is Javascript</h1></div>
-              )
-          }else if (this.state.activeTab === 2) {
-              return (
-                  <div><h1>this is dreamweaver</h1> </div>
-              )
-          }
-      }
+    
     render() {
-        return (
-            <div>
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab:tabId})} ripple>
-                    <Tab>HTML & CSS</Tab>
-                    <Tab>Javascript</Tab>
-                    <Tab>DreamWeaver</Tab>
+        return  (
 
-                </Tabs>
+     <div className="App">
+               
+        <header className="App-header">
+              
+              <h4> You can take a close look to my projects...<br/> just click to the code below. </h4> 
+               
+              <a href="https://github.com/EllaAb?tab=repositories" >
+               <img  src={projects} alt="projects"  className="App-logo" />
+              </a>
+           
+        </header>
+     </div>
 
-                </div>
         );
-    }
+       
+    };
 }
 
 export default Projects;
+
+
+
+
+/* constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+
+      if(this.state.activeTab === 0) {
+          return (
+       <div>  <h1>This Is html</h1></div> 
+          )
+      } else if(this.state.activeTab ===1) {
+          return (
+              <div><h1>This is Javascript</h1></div>
+          )
+      }else if (this.state.activeTab === 2) {
+          return (
+              <div><h1>this is dreamweaver</h1> </div>
+          )
+      }
+  } 
+
+
+  return (
+    <div>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab:tabId})} ripple>
+            <Tab>HTML & CSS</Tab>
+            <Tab>Javascript</Tab>
+            <Tab>DreamWeaver</Tab>
+
+        </Tabs>
+
+        </div>
+);  */
+
